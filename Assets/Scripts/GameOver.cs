3c6973ea466f17657de43f90f669f6bbb50133bf
Bad Chick Studios <lafiya@badchickstudios.com>
2023-12-29 15:49:16 -0500

    tweaked confetti and location prefabs

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -12,7 +12,12 @@ public class GameOver : MonoBehaviour
    // A simple script to make sure the confetti shows up in the right place when the game ends. :)
     public void WinGame()
     {
-        ObjectSpawner.SpawnObject(confetti, distance);
+       
+        var newPos = Camera.main.transform.TransformPoint(0 ,10f, 10f);
+        var newRot = Camera.main.transform.rotation;
+
+        confetti.transform.position = newPos;
+       // confetti.transform.rotation = newRot;
         confetti.SetActive(true);
     }
     
